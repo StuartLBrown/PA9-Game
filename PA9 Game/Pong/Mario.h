ed597464472eeeb54f04a732e22c4561c73a168c
mkilgore2000 <michael.kilgore@wsu.edu>
2019-04-17 13:27:33 -0700

    sdf

--- PA9 Game/Pong/Mario.h
+++ PA9 Game/Pong/Mario.h
@@ -1,8 +1,19 @@
-#pragma once
 #include <SFML/Graphics.hpp>
 
+//14 by 16 frames
 class Mario : public sf::Sprite {
 public:
-	Mario(const sf::Texture texture, const sf::IntRect &Size, float speed, float jumpHeight);
+	Mario(const sf::Texture &texture);
+	~Mario();
+	void Animate(int frame);
+	float getVelocityX(void);
+	float getVelocityY(void);
+	void setVelocityX(float newVelocityX);
+	void setVelocityY(float newVelocityY);
 private:
+	sf::IntRect Frames[14][16];
+	float velocityX;
+	float velocityY;
+	float accelerationX;
+	float accelerationY;
 };
--- PA9 Game/Pong/main.cpp
+++ PA9 Game/Pong/main.cpp
@@ -1,33 +1,85 @@
-#pragma once
+//http://xoxlabs.com/x/unity/2D_Mario_Clone/2D%20Mario%20Clone/Assets/2D%20Mario%20Assets/Textures/
+// This example will use SFML libraries to implement
+// the game of Pong
+
+// History: 4/12 - Designed a Paddle class. Have yet to 
+//                 instantiate one of these.
+//          4/10 - Designed and implemented a Ball class that's
+//                 derived from a sf::CircleShape.
+//          4/8 - Started from the SFML tutorial code that draws
+//                a circle in the window.
 #include <SFML/Graphics.hpp>
-#include "testmain.h"
+#include <iostream>
+#include <chrono>
+#include "Mario.h"
 
 int main()
 {
-	///*
-	//starting sample code
+	std::chrono::time_point<std::chrono::system_clock> time, time2;
+	time = std::chrono::system_clock::now();
 	sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
 	sf::Texture texture;
+	int animationNum = 0;
 	texture.loadFromFile("mario.png");
-	sf::Sprite sprite(texture);
+	Mario *mario = new Mario(texture);
 	while (window.isOpen())
 	{
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			switch (event.type)
+			{
+			case sf::Event::Closed:
 				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (event.key.code == sf::Keyboard::D)
+				{
+					animationNum = 4;
+					mario->setVelocityX(8);
+				}
+				if (event.key.code == sf::Keyboard::A)
+				{
+					animationNum = 5;
+					mario->setVelocityX(-8);
+				}
+				if (event.key.code == sf::Keyboard::Space && (animationNum == 4 || animationNum == 10))
+				{
+					animationNum = 10;
+					mario->setVelocityY(-8);
+				}
+				if (event.key.code == sf::Keyboard::Space && (animationNum == 5 || animationNum == 11))
+				{
+					animationNum = 11;
+					mario->setVelocityY(-8);
+				}
+				break;
+			case sf::Event::KeyReleased:
+				if (animationNum == 4)
+				{
+					mario->setVelocityX(0);
+					animationNum = 0;
+				}
+				if (animationNum == 5)
+				{
+					mario->setVelocityX(0);
+					animationNum = 1;
+				}
+				break;
+			}
 		}
+		mario->move(mario->getVelocityX(), mario->getVelocityY());
 
 		window.clear();
-		window.draw(shape);
+		time2 = std::chrono::system_clock::now();
+		std::chrono::duration<double> elapsed_seconds = time2 - time;
+		int timer = (int)(elapsed_seconds.count() * 100);
+		std::cout << elapsed_seconds.count() << std::endl;
+		if (timer % 15 == 0)
+			mario->Animate(animationNum);
+		window.draw(*mario);
 		window.display();
 	}
-	//*/
-	//test();
-	
+
 	return 0;
-	
 }
