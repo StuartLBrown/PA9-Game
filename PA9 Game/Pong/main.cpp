d9eb8962fd164e2c10a4cffb1b9e8fa434ced83f
Stuart Brown <solarmojo13@gmail.com>
2019-04-18 13:50:44 -0700

    solution to linker error in main

--- PA9 Game/Pong/main.cpp
+++ PA9 Game/Pong/main.cpp
@@ -1,4 +1,8 @@
 #include "Game.h"
+
+//fixing linker error, change in linker-input-additional dependencies change all the sfml-graphics.lib
+//to sfml-graphics-d.lib for all the dependencies
+
 int main()
 {
 	srand((unsigned int)time(NULL));
