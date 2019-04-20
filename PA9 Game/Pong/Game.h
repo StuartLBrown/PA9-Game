7404af93bdd4a48562ac03627be34f70d77054cc
StuartLBrown <stulwbrown@gmail.com>
2019-04-20 15:07:21 -0700

    Displaying stuff actually works now

--- PA9 Game/Pong/Game.h
+++ PA9 Game/Pong/Game.h
@@ -6,8 +6,11 @@
 #include "Mario.h"
 #include <chrono>
 
+
 using sf::Vector2f;
 using std::cout;
+
+void printVector(const Vector2f &v);
 class Game {
 private:
 	//temporary until we decide on what a player object will be named and more flushed out
@@ -53,22 +56,22 @@ Game::Game() {
 				window.close();
 				break;
 			case sf::Event::KeyPressed:
-				if (event.key.code == sf::Keyboard::D&&camera.getCenter().x - mario->getPosition().x <= camera.getSize().x / 2 - 50) {
+				if (event.key.code == sf::Keyboard::D) {
 					animationNum = 4;
-					mario->setVelocityX(8);
+					mario->setVelocityX(1);
 				}
-				if (event.key.code == sf::Keyboard::A) {
+				if (event.key.code == sf::Keyboard::A&&camera.getCenter().x - mario->getPosition().x <= camera.getSize().x / 2) {
 					animationNum = 5;
-					mario->setVelocityX(-8);
+					mario->setVelocityX(-1);
 				}
 				if (event.key.code == sf::Keyboard::Space&&mario->getPosition().y >= 0) {
-					if (animationNum == 4 || animationNum == 10) {
-						animationNum = 10;
-					}
-					if (animationNum == 5 || animationNum == 11) {
-						animationNum = 11;
-					}
 					mario->setVelocityY(-8);
+					animationNum = 10;
+					if (mario->getPosition().y == 500)
+					{
+						mario->setVelocityY(-3);
+						mario->setAccelerationY(1);
+					}
 				}
 				break;
 			case sf::Event::KeyReleased:
@@ -105,16 +108,25 @@ Game::Game() {
 		time2 = std::chrono::system_clock::now();
 		std::chrono::duration<double> elapsed_seconds = time2 - time;
 		int timer = (int)(elapsed_seconds.count() * 100);
-		std::cout << elapsed_seconds.count() << std::endl;
+		printVector(mario->getPosition());
 		if (timer % 15 == 0)
 			mario->Animate(animationNum);
-
+		if (timer % 45 == 0){
+			if (mario->getPosition().y < 500)
+				mario->adjustVelocity();
+		}
+		if (mario->getPosition().y > 500){
+			mario->setPosition(mario->getPosition().x, 500);
+			mario->setAccelerationY(0);
+			mario->setVelocityY(0);
+			animationNum = 1;
+		}
 		window.clear();
 
 		window.setView(camera);
 
-		window.draw(*mario);
 		window.draw(background);
+		window.draw(*mario);
 
 		window.display();
 	}
@@ -131,4 +143,4 @@ void Game::populate(std::list<sf::RectangleShape>& objects, float x, float y)
 }
 
 //information function, should only be used for testing purposes
-void printVector(Vector2f &v) { cout << "X Position: " << v.x << " | Y Position: " << v.y << std::endl; }
+void printVector(const Vector2f &v) { cout << "X Position: " << v.x << " | Y Position: " << v.y << std::endl; }
